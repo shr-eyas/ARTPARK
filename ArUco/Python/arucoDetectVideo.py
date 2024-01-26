7d14b76f090fcfaaef7a9ca9265cb7a3c604cf91
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2024-01-26 22:51:29 +0530

    Update arucoDetectVideo.py

--- ArUco/Python/arucoDetectVideo.py
+++ ArUco/Python/arucoDetectVideo.py
@@ -3,34 +3,24 @@
 
 import cv2
 
-cap = cv2.VideoCapture(0)
-
-def findAruco(image, draw=True):
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-    parameters =  cv2.aruco.DetectorParameters_create()
-
-    corners, ids, _ = cv2.aruco.detectMarkers(gray, dictionary, parameters=parameters)
-
-    print(ids)
-
-    if draw:
-        cv2.aruco.drawDetectedMarkers(image,corners,ids)
-
-    return corners, ids
+cap = cv2.VideoCapture(0)  
 
 while cap.isOpened():
- 
-    _, image = cap.read()
+    _, image = cap.read() 
 
-    corners, ids = findAruco(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters_create()  
 
-    cv2.imshow('image',image)
+    corners, ids, _ = cv2.aruco.detectMarkers(gray, dictionary) 
+    print(ids)  
 
-    key = cv2.waitKey(1)
-    if key == 27:
+    cv2.aruco.drawDetectedMarkers(image, corners, ids)  
+    cv2.imshow('image', image)  
+
+    key = cv2.waitKey(1)  
+    if key == 27:  
         break
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release() 
+cv2.destroyAllWindows()  
