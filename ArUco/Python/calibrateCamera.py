c10f47f16d28e52c949db0b6e763dca628e0b503
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2024-01-26 20:32:36 +0530

    Update calibrateCamera.py

--- ArUco/Python/calibrateCamera.py
+++ ArUco/Python/calibrateCamera.py
@@ -1,41 +1,77 @@
 import cv2
 import numpy as np
+import os
 import glob
-
-CHECKERBOARD = (9, 6)  # Adjust dimensions based on your checkerboard
+ 
+# Defining the dimensions of checkerboard
+CHECKERBOARD = (8,6)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+# Creating vector to store vectors of 3D points for each checkerboard image
 objpoints = []
-imgpoints = []
-objp = None
-camera_matrix = None
-distortion_coefficients = None
-
-folder_path = "path/to/your/checkerboard/images"
-images = glob.glob(folder_path + '/*.png')
-
-for file in images:
-    image = cv2.imread(file)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-    objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-
-    ret, corners = cv2.findChessboardCorners(
-        gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
-
-    if ret:
-        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+# Creating vector to store vectors of 2D points for each checkerboard image
+imgpoints = [] 
+ 
+ 
+# Defining the world coordinates for 3D points
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+prev_img_shape = None
+ 
+# Extracting path of individual image stored in a given directory
+images = glob.glob('/home/sophia/Documents/ARTPARK/Calibration/*.png')
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    # If desired number of corners are found in the image then ret = true
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+     
+    """
+    If desired number of corner are detected,
+    we refine the pixel coordinates and display 
+    them on the images of checker board
+    """
+    if ret == True:
         objpoints.append(objp)
+        # refining pixel coordinates for given 2d points.
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+         
         imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+     
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
+ 
+cv2.destroyAllWindows()
+ 
+h,w = img.shape[:2]
+ 
+"""
+Performing camera calibration by 
+passing the value of known 3D points (objpoints)
+and corresponding pixel coordinates of the 
+detected corners (imgpoints)
+"""
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ 
+print("Camera matrix : \n")
+print(mtx)
+print("dist : \n")
+print(dist)
+print("rvecs : \n")
+print(rvecs)
+print("tvecs : \n")
+print(tvecs)
 
-h, w = image.shape[:2]
+# Camera matrix : 
 
-ret, camera_matrix, distortion_coefficients, _, _ = cv2.calibrateCamera(
-    objpoints, imgpoints, (w, h), None, None)
+# [[936.06532138   0.         633.76194261]
+#  [  0.         937.35446125 344.36732656]
+#  [  0.           0.           1.        ]]
+# dist : 
 
-if ret:
-    print("Calibration successful!")
-    print("Camera Matrix:\n", camera_matrix)
-    print("Distortion Coefficients:\n", distortion_coefficients)
-else:
-    print("Calibration failed.")
+# [[ 3.02920501e-01 -1.44159267e+00 -2.20298502e-03 -6.58754371e-03
+#   -3.53050764e+00]]
