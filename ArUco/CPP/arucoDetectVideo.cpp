3df2be6f9f823166ab6b70713cd54f91156198af
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2024-01-26 22:43:10 +0530

    Update arucoDetectVideo.cpp

--- ArUco/CPP/arucoDetectVideo.cpp
+++ ArUco/CPP/arucoDetectVideo.cpp
@@ -1,71 +1,34 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-using namespace cv;
+#include <opencv2/aruco.hpp>
 
 int main() {
-    // Defining the dimensions of the checkerboard
-    Size CHECKERBOARD(8, 6);
-    TermCriteria criteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 30, 0.001);
 
-    // Creating vectors to store vectors of 3D and 2D points
-    vector<vector<Point3f>> objpoints;
-    vector<vector<Point2f>> imgpoints;
+    cv::VideoCapture cap(0);
 
-    // Defining the world coordinates for 3D points
-    vector<Point3f> objp;
-    for (int i = 0; i < CHECKERBOARD.height; i++) {
-        for (int j = 0; j < CHECKERBOARD.width; j++) {
-            objp.push_back(Point3f(j, i, 0));
+    cv::Mat image;
+
+    while (cap.isOpened()){
+        cap >> image;
+        cv::Mat gray;
+        cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+        cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+        std::vector<std::vector<cv::Point2f>> corners;
+        std::vector<int> ids;
+        cv::aruco::detectMarkers(gray, dictionary, corners, ids);
+
+        if (!ids.empty()) {
+            cv::aruco::drawDetectedMarkers(image, corners, ids);
+        }
+        cv::imshow("image", image);
+
+        int key = cv::waitKey(1);
+        if (key == 27) {
+            break;
         }
     }
 
-    // Extracting path of individual images stored in a given directory
-    vector<String> images;
-    glob("/home/sophia/Documents/Calibration/*.png", images);
-
-    Mat gray;
-    for (size_t i = 0; i < images.size(); i++) {
-        Mat img = imread(images[i]);
-        cvtColor(img, gray, COLOR_BGR2GRAY);
-
-        // Find the chessboard corners
-        vector<Point2f> corners;
-        bool patternFound = findChessboardCorners(gray, CHECKERBOARD, corners,
-            CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_FAST_CHECK + CALIB_CB_NORMALIZE_IMAGE);
-
-        // If desired number of corners are found in the image, then patternFound = true
-        if (patternFound) {
-            objpoints.push_back(objp);
-
-            // Refining pixel coordinates for given 2D points
-            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), criteria);
-            imgpoints.push_back(corners);
-
-            // Draw and display the corners
-            drawChessboardCorners(img, CHECKERBOARD, corners, patternFound);
-
-            imshow("img", img);
-            waitKey(0);
-        }
-    }
-
-    destroyAllWindows();
-
-    // Get image size
-    Size imageSize(gray.cols, gray.rows);
-
-    // Performing camera calibration
-    Mat cameraMatrix, distCoeffs;
-    vector<Mat> rvecs, tvecs;
-    double rms = calibrateCamera(objpoints, imgpoints, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
-
-    cout << "Camera matrix : \n" << cameraMatrix << endl;
-    cout << "Distortion coefficients : \n" << distCoeffs << endl;
+    cap.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
